32159790eaeb53a04e4001437d5ec85018ed7db0
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-02-02 16:43:19 +0100

    try to autoplay background music (failed)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Logo from "./components/logo/Logo.js"
 
 import ElevatorIMG from "./assets/img/background.png"
 import CharacterIMG from "./assets/img/character2.png"
+import Audio from './assets/audio/keys-of-moon-immense.mp3';
 
 function App() {
   return (
     <div className="app">
+      {/* <audio autoplay>
+        <source src={Audio} type="audio/mpeg"/>
+      </audio> */}
       <div className="wrapper-parallax">
         <header>
           <div className='wrapper-logo'>
@@ -19,6 +23,26 @@ function App() {
           <img className="character" src={CharacterIMG} alt="solid snake" />
       </header>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil minus sapiente ullam. Ab ad iste non, laborum id qui tempore cumque voluptatem! Ab dicta sequi corrupti? Iusto, dignissimos cumque? Provident!
+          Sit, ea eius necessitatibus nam est impedit corrupti fugiat, vero et tempore officia reiciendis numquam? Sequi perferendis voluptas voluptatem veniam voluptatum rem odio tenetur nesciunt nulla consequatur saepe, facilis accusamus.
+          Porro omnis deleniti dolor at minima dignissimos laborum excepturi itaque molestias. Harum cupiditate quam temporibus maiores earum omnis, cum, voluptatum unde suscipit perspiciatis voluptate nostrum ipsam assumenda vitae id. Voluptatibus.
+          Architecto, facere. Aperiam, voluptas officiis molestiae ipsum vitae illo exercitationem eius facilis eum, quaerat assumenda. Fuga dicta deserunt aliquam totam facere repellendus ullam omnis illum vel autem, asperiores eos? Exercitationem?
+          Assumenda commodi voluptatibus natus harum aperiam dolorum exercitationem at. Minus quaerat quis accusamus dolore consectetur nisi vel similique eligendi amet unde tenetur in sunt, recusandae placeat dignissimos exercitationem animi atque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil minus sapiente ullam. Ab ad iste non, laborum id qui tempore cumque voluptatem! Ab dicta sequi corrupti? Iusto, dignissimos cumque? Provident!
+          Sit, ea eius necessitatibus nam est impedit corrupti fugiat, vero et tempore officia reiciendis numquam? Sequi perferendis voluptas voluptatem veniam voluptatum rem odio tenetur nesciunt nulla consequatur saepe, facilis accusamus.
+          Porro omnis deleniti dolor at minima dignissimos laborum excepturi itaque molestias. Harum cupiditate quam temporibus maiores earum omnis, cum, voluptatum unde suscipit perspiciatis voluptate nostrum ipsam assumenda vitae id. Voluptatibus.
+          Architecto, facere. Aperiam, voluptas officiis molestiae ipsum vitae illo exercitationem eius facilis eum, quaerat assumenda. Fuga dicta deserunt aliquam totam facere repellendus ullam omnis illum vel autem, asperiores eos? Exercitationem?
+          Assumenda commodi voluptatibus natus harum aperiam dolorum exercitationem at. Minus quaerat quis accusamus dolore consectetur nisi vel similique eligendi amet unde tenetur in sunt, recusandae placeat dignissimos exercitationem animi atque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil minus sapiente ullam. Ab ad iste non, laborum id qui tempore cumque voluptatem! Ab dicta sequi corrupti? Iusto, dignissimos cumque? Provident!
+          Sit, ea eius necessitatibus nam est impedit corrupti fugiat, vero et tempore officia reiciendis numquam? Sequi perferendis voluptas voluptatem veniam voluptatum rem odio tenetur nesciunt nulla consequatur saepe, facilis accusamus.
+          Porro omnis deleniti dolor at minima dignissimos laborum excepturi itaque molestias. Harum cupiditate quam temporibus maiores earum omnis, cum, voluptatum unde suscipit perspiciatis voluptate nostrum ipsam assumenda vitae id. Voluptatibus.
+          Architecto, facere. Aperiam, voluptas officiis molestiae ipsum vitae illo exercitationem eius facilis eum, quaerat assumenda. Fuga dicta deserunt aliquam totam facere repellendus ullam omnis illum vel autem, asperiores eos? Exercitationem?
+          Assumenda commodi voluptatibus natus harum aperiam dolorum exercitationem at. Minus quaerat quis accusamus dolore consectetur nisi vel similique eligendi amet unde tenetur in sunt, recusandae placeat dignissimos exercitationem animi atque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil minus sapiente ullam. Ab ad iste non, laborum id qui tempore cumque voluptatem! Ab dicta sequi corrupti? Iusto, dignissimos cumque? Provident!
+          Sit, ea eius necessitatibus nam est impedit corrupti fugiat, vero et tempore officia reiciendis numquam? Sequi perferendis voluptas voluptatem veniam voluptatum rem odio tenetur nesciunt nulla consequatur saepe, facilis accusamus.
+          Porro omnis deleniti dolor at minima dignissimos laborum excepturi itaque molestias. Harum cupiditate quam temporibus maiores earum omnis, cum, voluptatum unde suscipit perspiciatis voluptate nostrum ipsam assumenda vitae id. Voluptatibus.
+          Architecto, facere. Aperiam, voluptas officiis molestiae ipsum vitae illo exercitationem eius facilis eum, quaerat assumenda. Fuga dicta deserunt aliquam totam facere repellendus ullam omnis illum vel autem, asperiores eos? Exercitationem?
+          Assumenda commodi voluptatibus natus harum aperiam dolorum exercitationem at. Minus quaerat quis accusamus dolore consectetur nisi vel similique eligendi amet unde tenetur in sunt, recusandae placeat dignissimos exercitationem animi atque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil minus sapiente ullam. Ab ad iste non, laborum id qui tempore cumque voluptatem! Ab dicta sequi corrupti? Iusto, dignissimos cumque? Provident!
+          Sit, ea eius necessitatibus nam est impedit corrupti fugiat, vero et tempore officia reiciendis numquam? Sequi perferendis voluptas voluptatem veniam voluptatum rem odio tenetur nesciunt nulla consequatur saepe, facilis accusamus.
+          Porro omnis deleniti dolor at minima dignissimos laborum excepturi itaque molestias. Harum cupiditate quam temporibus maiores earum omnis, cum, voluptatum unde suscipit perspiciatis voluptate nostrum ipsam assumenda vitae id. Voluptatibus.
+          Architecto, facere. Aperiam, voluptas officiis molestiae ipsum vitae illo exercitationem eius facilis eum, quaerat assumenda. Fuga dicta deserunt aliquam totam facere repellendus ullam omnis illum vel autem, asperiores eos? Exercitationem?
+          Assumenda commodi voluptatibus natus harum aperiam dolorum exercitationem at. Minus quaerat quis accusamus dolore consectetur nisi vel similique eligendi amet unde tenetur in sunt, recusandae placeat dignissimos exercitationem animi atque.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil minus sapiente ullam. Ab ad iste non, laborum id qui tempore cumque voluptatem! Ab dicta sequi corrupti? Iusto, dignissimos cumque? Provident!
           Sit, ea eius necessitatibus nam est impedit corrupti fugiat, vero et tempore officia reiciendis numquam? Sequi perferendis voluptas voluptatem veniam voluptatum rem odio tenetur nesciunt nulla consequatur saepe, facilis accusamus.
           Porro omnis deleniti dolor at minima dignissimos laborum excepturi itaque molestias. Harum cupiditate quam temporibus maiores earum omnis, cum, voluptatum unde suscipit perspiciatis voluptate nostrum ipsam assumenda vitae id. Voluptatibus.
           Architecto, facere. Aperiam, voluptas officiis molestiae ipsum vitae illo exercitationem eius facilis eum, quaerat assumenda. Fuga dicta deserunt aliquam totam facere repellendus ullam omnis illum vel autem, asperiores eos? Exercitationem?
