6da37230f9e34ca85c4153c6bcaab22e9051f66b
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-02-02 15:00:59 +0100

    implement the logo component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Logo from "./components/logo/Logo.js"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <p>hallo</p>
+      <div className='wrapper-logo'>
+        <Logo
+        title="METAL GEAR"
+        subtitle="SOLID"
+        genre="TACTICAL ESPIONAGE ACTION" />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/components/logo/Logo.js
@@ -0,0 +1,13 @@
+import "./Logo.Module.css"
+
+const Logo = ({title, subtitle, genre}) => {
+    return (  
+        <div className="logo">
+            <h3>{genre}</h3>
+            <h1>{title}</h1>
+            <h2>{subtitle}</h2>
+        </div>
+    );
+}
+ 
+export default Logo;
