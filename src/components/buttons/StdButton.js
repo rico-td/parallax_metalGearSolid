4bf389e644e7a9a452686b9878d9bb079f0ee27b
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-02-04 17:01:45 +0100

    *

--- src/components/buttons/StdButton.js
+++ src/components/buttons/StdButton.js
@@ -1,14 +1,13 @@
-import "./StdButton.Module.css"
+import "./StdButton.Module.css";
 
-const StdButton = ({source, text}) => {
-    return ( 
-        <div class="container-Btn">
-            <a href={source} target="_blank" rel="noreferrer">
-                <button className="btn">{text}</button>
-            </a>
-        </div>
+const StdButton = ({ source, text }) => {
+  return (
+    <div class="container-Btn">
+      <a href={source} target="_blank" rel="noreferrer">
+        <button className="btn">{text}</button>
+      </a>
+    </div>
+  );
+};
 
-     );
-}
- 
 export default StdButton;
