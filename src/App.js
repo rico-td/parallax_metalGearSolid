0fa858cd7a153f974c3710cc519ee1b9688652d6
Rico.TechDev <151479600+rico-td@users.noreply.github.com>
2024-02-04 04:05:29 +0100

    working on parallax and logo (resp. Design)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles/media.queries.css";
 import "./components/logo/Logo.Module.css";
 import Logo from "./components/logo/Logo.js";
 
-import ElevatorIMG from "./assets/img/background.png";
-import CharacterIMG from "./assets/img/character2.png";
+import elevatorBg from "./assets/img/background.png";
+import elevator from "./assets/img/elevator.png";
+import character from "./assets/img/character2.png";
 import Navbar from "./components/navbar/Navbar.js";
 
 function App() {
@@ -15,33 +16,20 @@ function App() {
         <header>
           {/* noch anschauen: Position des Logos richtig setzen, responsive! */}
           <div className="wrapper-logo">
-            <div className="containter3D">
-              <Logo
-                title="METAL GEAR"
-                subtitle="SOLID"
-                genre="TACTICAL ESPIONAGE ACTION"
-              />
-            </div>
+            <Logo
+              title="METAL GEAR"
+              subtitle="SOLID"
+              genre="TACTICAL ESPIONAGE ACTION"
+            />
           </div>
-          <img className="elevator" src={ElevatorIMG} alt="elevator" />
-          <img className="character" src={CharacterIMG} alt="solid snake" />
+          <img
+            className="elevator-bg"
+            src={elevatorBg}
+            alt="background elevator"
+          />
+          <img className="elevator" src={elevator} alt="elevator" />
+          <img className="character" src={character} alt="solid snake" />
         </header>
-        <article>
-          <h3>The Threat of FOXHOUND</h3>
-          <p>
-            In the immersive world of Metal Gear Solid 1, the threat of FOXHOUND
-            looms large. Solid Snake, a legendary soldier, is called back into
-            action by the enigmatic Colonel Campbell to investigate suspicious
-            activities on Shadow Moses Island. As Snake delves deeper into the
-            heart of the island, he uncovers a web of deceit and intrigue, with
-            FOXHOUND at its center. The threat of a nuclear catastrophe hangs
-            heavy in the air, driving Snake to confront his past and question
-            his allegiances. Amidst the shadowy corridors and hidden dangers of
-            Shadow Moses, Snake must navigate a treacherous landscape of
-            betrayal and danger to uncover the truth and save the world from
-            impending doom.
-          </p>
-        </article>
         <article>
           <h3>The Threat of FOXHOUND</h3>
           <p>
